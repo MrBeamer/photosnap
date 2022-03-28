aa3475d57e52cde0da4a5727693f33db108249eb
michael beamer <beamermichael@gmail.com>
2022-03-28 13:47:01 +0200

    refactor infoCard

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -6,19 +6,23 @@ export default function InfoCard() {
   return home.map((item, index) => {
     return (
       <div className={styles.container} key={index}>
-        <div className={styles.contentContainer}>
-          <h2 className="headline--big">{item.title}</h2>
-          <p className={styles.paragraph}>{item.desc}</p>
-          <Button type="arrowWhite" to="/">
-            {item.link}
-          </Button>
+        <div className={styles.contentWrapper}>
+          <div className={styles.contentContainer}>
+            <h2 className="headline--big">{item.title}</h2>
+            <p className={styles.paragraph}>{item.desc}</p>
+            <Button type="arrowWhite" to="/">
+              {item.link}
+            </Button>
+          </div>
         </div>
-        <picture>
-          <source media="(max-width: 480px)" srcSet={item.img.mobile} />
-          <source media="(max-width: 768px)" srcSet={item.img.tablet} />
-          <source media="(max-width: 1440px)" srcSet={item.img.desktop} />
-          <img src={item.img.mobile} alt="mobile" />
-        </picture>
+        <div className={styles.imgContainer}>
+          <picture>
+            <source media="(max-width: 480px)" srcSet={item.img.mobile} />
+            <source media="(max-width: 768px)" srcSet={item.img.tablet} />
+            <source media="(max-width: 1440px)" srcSet={item.img.desktop} />
+            <img src={item.img.mobile} alt="mobile" />
+          </picture>
+        </div>
       </div>
     );
   });
