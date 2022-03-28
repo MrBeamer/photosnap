88762e4830240b74e2edc3deb18d1c07c61e3dd6
michael beamer <beamermichael@gmail.com>
2022-03-28 19:51:27 +0200

    added gallery data

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,18 +5,34 @@ import Button from "../button/Button";
 export default function Gallery() {
   return (
     <div className={styles.grid}>
-      <div
-        className={styles.background}
-        styles={{ backgroundImage: `url(${gallery.img})` }}
-      >
-        <p className={styles.date}>{gallery.date}</p>
-        <h2 className="headline--medium">{gallery.title}</h2>
-        <p className={styles.author}>{gallery.author}</p>
-        <Button to={gallery.target}>{gallery.link}</Button>
-      </div>
-      {/* <img src={gallery.img} alt={gallery.title}></img> */}
+      {gallery.map((item, index) => {
+        return (
+          <div
+            key={index}
+            style={{
+              backgroundImage: `url(${item.img.desktop})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          >
+            <p className={styles.date}>{item.date}</p>
+            <h2 className="headline--medium">{item.title}</h2>
+            <p className={styles.author}>{item.author}</p>
+            <Button to={item.target}>{item.link}</Button>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-//maybe use background instead of image
+/* //maybe use background instead of image */
+{
+  /* <picture>
+<source media="(max-width: 480px)" srcSet={item.img.mobile} />
+<source media="(max-width: 768px)" srcSet={item.img.tablet} />
+<source media="(max-width: 1440px)" srcSet={item.img.desktop} />
+<img src={item.img.mobile} alt="mobile" />
+</picture> */
+}
--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
+import moonD from "../src/assets/stories/desktop/moon-of-appalacia.jpg";
+import moonT from "../src/assets/stories/tablet/moon-of-appalacia.jpg";
+import moonM from "../src/assets/stories/tablet/moon-of-appalacia.jpg";
+
 import createD from "../src/assets/home/desktop/create-and-share.jpg";
 import createT from "../src/assets/home/tablet/create-and-share.jpg";
 import createM from "../src/assets/home/mobile/create-and-share.jpg";
@@ -7,6 +11,218 @@ import storiesM from "../src/assets/home/mobile/beautiful-stories.jpg";
 import designD from "../src/assets/home/desktop/designed-for-everyone.jpg";
 import designT from "../src/assets/home/tablet/designed-for-everyone.jpg";
 import designM from "../src/assets/home/mobile/designed-for-everyone.jpg";
+
+import mountainD from "../src/assets/stories/desktop/mountains.jpg";
+import mountainM from "../src/assets/stories/mobile/mountains.jpg";
+import cityD from "../src/assets/stories/desktop/cityscapes.jpg";
+import cityM from "../src/assets/stories/mobile/cityscapes.jpg";
+import dayD from "../src/assets/stories/desktop/18-days-voyage.jpg";
+import dayM from "../src/assets/stories/mobile/18-days-voyage.jpg";
+import archiD from "../src/assets/stories/desktop/architecturals.jpg";
+import archiM from "../src/assets/stories/desktop/architecturals.jpg";
+import waveD from "../src/assets/stories/desktop/behind-the-waves.jpg";
+import waveM from "../src/assets/stories/mobile/behind-the-waves.jpg";
+import waterM from "../src/assets/stories/mobile/calm-waters.jpg";
+import waterD from "../src/assets/stories/desktop/calm-waters.jpg";
+import forestD from "../src/assets/stories/desktop/dark-forest.jpg";
+import forestM from "../src/assets/stories/mobile/dark-forest.jpg";
+import kingD from "../src/assets/stories/desktop/king-on-africa.jpg";
+import kingM from "../src/assets/stories/mobile/king-on-africa.jpg";
+import landM from "../src/assets/stories/mobile/land-of-dreams.jpg";
+import landD from "../src/assets/stories/desktop/land-of-dreams.jpg";
+import milkD from "../src/assets/stories/desktop/milky-way.jpg";
+import milkM from "../src/assets/stories/mobile/milky-way.jpg";
+import seaD from "../src/assets/stories/desktop/rage-of-the-sea.jpg";
+import seaM from "../src/assets/stories/mobile/rage-of-the-sea.jpg";
+import runD from "../src/assets/stories/desktop/running-free.jpg";
+import runM from "../src/assets/stories/mobile/running-free.jpg";
+import petD from "../src/assets/stories/desktop/somwarpet.jpg";
+import petM from "../src/assets/stories/mobile/somwarpet.jpg";
+import tripD from "../src/assets/stories/desktop/trip-to-nowhere.jpg";
+import tripM from "../src/assets/stories/mobile/trip-to-nowhere.jpg";
+import worldD from "../src/assets/stories/desktop/world-tour.jpg";
+import worldM from "../src/assets/stories/mobile/world-tour.jpg";
+import cornerD from "../src/assets/stories/desktop/unforeseen-corners.jpg";
+import cornerM from "../src/assets/stories/mobile/unforeseen-corners.jpg";
+
+export const gallery = [
+  {
+    date: "April 16th 2020",
+    title: "The Mountains",
+    img: {
+      desktop: mountainD,
+      mobile: mountainM,
+    },
+    author: "by John Appleseed",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "April 14th 2020",
+    title: "Sunset Cityscapes",
+    img: {
+      desktop: cityD,
+      mobile: cityM,
+    },
+    author: "by Benjamin Cruz",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "April 11th 2020",
+    title: "18 Days Voyage",
+    img: {
+      desktop: dayD,
+      mobile: dayM,
+    },
+    author: "by Alexei Borodin",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "April 9th 2020",
+    title: "Architecturals",
+    img: {
+      desktop: archiD,
+      mobile: archiM,
+    },
+    author: "by Samantha Brooke",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "April 7th 2020",
+    title: "World Tour 2019",
+    img: {
+      desktop: worldD,
+      mobile: worldM,
+    },
+    author: "by Timothy Wagner",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "April 3rd 2020",
+    title: "Unforeseen Corners",
+    img: {
+      desktop: cornerD,
+      mobile: cornerM,
+    },
+    author: "by William Malcolm",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 29th 2020",
+    title: "King on Africa: Part II",
+    img: {
+      desktop: kingD,
+      mobile: kingM,
+    },
+    author: "by Tim Hillenburg",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 21st 2020",
+    title: "The Trip to Nowhere",
+    img: {
+      desktop: tripD,
+      mobile: tripM,
+    },
+    author: "by Felicia Rourke",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 19th 2020",
+    title: "Rage of The Sea",
+    img: {
+      desktop: seaD,
+      mobile: seaM,
+    },
+    author: "by Mohammed Abdul",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 16th 2020",
+    title: "Running Free",
+    img: {
+      desktop: runD,
+      mobile: runM,
+    },
+    author: "by Michelle",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 11th 2020",
+    title: "Behind the Waves",
+    img: {
+      desktop: waveD,
+      mobile: waveM,
+    },
+    author: "by Lamarr Wilson",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 9th 2020",
+    title: "Calm Waters",
+    img: {
+      desktop: waterD,
+      mobile: waterM,
+    },
+    author: "by Samantha Brooke",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 5th 2020",
+    title: "The Milky Way",
+    img: {
+      desktop: milkD,
+      mobile: milkM,
+    },
+    author: "by Benjamin Cruz",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 4th 2020",
+    title: "Night at The Dark Forest",
+    img: {
+      desktop: forestD,
+      mobile: forestM,
+    },
+    author: "by  Mohammed Abdul",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "March 1st 2020",
+    title: "Somwarpet’s Beauty",
+    img: {
+      desktop: petD,
+      mobile: petM,
+    },
+    author: "by Michelle",
+    link: "READ STORY",
+    target: "/",
+  },
+  {
+    date: "February 25th 2020",
+    title: "Land of Dreams",
+    img: {
+      desktop: landD,
+      mobile: landM,
+    },
+    author: "by William Malcolm",
+    link: "READ STORY",
+    target: "/",
+  },
+];
 
 export const home = [
   {
@@ -41,200 +257,5 @@ export const home = [
     desc: "Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.",
     link: "VIEW THE STORIES",
     target: "/stories",
-  },
-];
-
-export const gallery = [
-  {
-    date: "April 16th 2020",
-    title: "The Mountains",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by John Appleseed",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "April 14th 2020",
-    title: "Sunset Cityscapes",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Benjamin Cruz",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "April 11th 2020",
-    title: "18 Days Voyage",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Alexei Borodin",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "April 9th 2020",
-    title: "Architecturals",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Samantha Brooke",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "April 7th 2020",
-    title: "World Tour 2019",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Timothy Wagner",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "April 3rd 2020",
-    title: "Unforeseen Corners",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by William Malcolm",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 29th 2020",
-    title: "King on Africa: Part II",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Tim Hillenburg",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 21st 2020",
-    title: "The Trip to Nowhere",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Felicia Rourke",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 19th 2020",
-    title: "Rage of The Sea",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Mohammed Abdul",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 16th 2020",
-    title: "Running Free",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Michelle",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 11th 2020",
-    title: "Behind the Waves",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Lamarr Wilson",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 9th 2020",
-    title: "Calm Waters",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Samantha Brooke",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 5th 2020",
-    title: "The Milky Way",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Benjamin Cruz",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 4th 2020",
-    title: "Night at The Dark Forest",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by  Mohammed Abdul",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "March 1st 2020",
-    title: "Somwarpet’s Beauty",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by Michelle",
-    link: "READ STORY",
-    target: "/",
-  },
-  {
-    date: "February 25th 2020",
-    title: "Land of Dreams",
-    img: {
-      desktop: createD,
-      tablet: createT,
-      mobile: createM,
-    },
-    author: "by William Malcolm",
-    link: "READ STORY",
-    target: "/",
   },
 ];
