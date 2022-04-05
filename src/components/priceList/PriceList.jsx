8fabeaa7c103cce5682c3e249fdbb7a87d3aabab
michael beamer <beamermichael@gmail.com>
2022-04-05 23:51:23 +0200

    fixed active priceCard

--- src/components/priceList/PriceList.jsx
+++ src/components/priceList/PriceList.jsx
@@ -1,11 +1,29 @@
 import styles from "./priceList.module.css";
 import PriceCard from "../priceCard/PriceCard";
+import { useState } from "react";
 
 export default function PriceList({ pricing, type }) {
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  function handleSpotlightClick(index) {
+    setSelectedIndex(index);
+    console.log(index);
+  }
+
+  console.log(selectedIndex);
+
   return (
     <div className={styles.container}>
       {pricing.map((item, index) => {
-        return <PriceCard key={index} item={item} type={type} />;
+        return (
+          <PriceCard
+            key={index}
+            item={item}
+            type={type}
+            onSpotlightClick={() => handleSpotlightClick(index)}
+            selected={index === selectedIndex}
+          />
+        );
       })}
     </div>
   );
